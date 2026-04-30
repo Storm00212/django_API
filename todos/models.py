999e51b1ea1233ceaab5514e885d1b903a5859d0
Storm00212 <paulmuyalikhams@gmail.com>
2026-04-30 14:25:34 +0300

    MODELS

---
+++ todos/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
